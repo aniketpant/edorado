f239bed358d441c7d647c8d3afc6329ab650854e
Aniket Pant <me@aniketpant.com>
2013-01-15 22:40:44 +0530

    Changed: visual improvements
    Signed-off-by: Aniket Pant <me@aniketpant.com>

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -165,7 +165,11 @@ class Home extends CI_Controller {
                 if ($this->session->userdata('logged_in')) {
                     $data['page_title'] = 'Wait for it';
                     $this->load->helper('date');
-                    $data['remaining'] = unix_to_human((human_to_unix('21/01/2013') - now()), TRUE, 'us');
+                    $start = human_to_unix('2013-01-15 06:00 PM');
+                    $now = gmt_to_local(now(), 'UP45', TRUE);
+                    $end = human_to_unix('2013-01-21 00:00 AM');
+                    $data['bar'] = ($now - $start)/($end - $start) * 100;
+                    $data['start'] = timespan($now, $end);
                     $this->load->view('user/wait-for-it', $data);
                 }
                 else {
--- application/views/user/wait-for-it.php
+++ application/views/user/wait-for-it.php
@@ -2,14 +2,14 @@
 
 	<div class="hero-unit">
 	    <h1 class="page-header">Hang on mate!</h1>
-	    <p>Hold your horses till <strong>21st Jan, 00:00 Hrs</strong>.</p>
-      <p>Time to go <?= $remaining ?></p>
+	    <p>Hold your horses for <strong><?= $start ?></strong>.</p>
+      <p>Go get your friends till then. Won't it be great to have more people.</p>
 	</div>
 
   <h2>We're nearly set.</h2>
 
-  <div class="progress progress-striped active">
-    <div class="bar" style="width: 85%;"></div>
+  <div class="progress progress-striped active progress-success">
+    <div class="bar" style="width: <?= $bar ?>%;"></div>
   </div>
 
   <?php if(!$is_complete) { ?>
